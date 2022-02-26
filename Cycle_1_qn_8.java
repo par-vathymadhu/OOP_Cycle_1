753f2966c32e90a4167695efca3e01ae91851b6a
Parvathy Madhu <98249476+par-vathymadhu@users.noreply.github.com>
2022-02-26 11:29:54 +0530

    Update Cycle_1_qn_8.java

--- Cycle_1_qn_8.java
+++ Cycle_1_qn_8.java
@@ -1,3 +1,5 @@
+/**Program to find factorial of a given number**/
+
 import java.util.*;
 public class Cycle_1_qn_8 
 {
@@ -5,10 +7,18 @@ public class Cycle_1_qn_8
     {
         Scanner qn8=new Scanner(System.in);
         int fact=1;
-        System.out.println("Enter the number whose factorial needs to be found: ");
+        System.out.print("Enter the number whose factorial needs to be found: ");
         int num=qn8.nextInt();
         for(int i=1;i<=num;i++)
             fact=fact*i;
         System.out.println("Factorial of "+num+" : "+fact);
     }    
 }
+
+/**
+Output:
+
+Enter the number whose factorial needs to be found: 4
+Factorial of 4 : 24
+
+**/
